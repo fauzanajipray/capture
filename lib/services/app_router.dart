65d3917868de405327688b5af30668688b2eee5b
Fauzan Ajip <fauzan.aji@rectmedia.id>
2023-12-30 18:01:02 +0700

    Add bottom navigation

--- lib/services/app_router.dart
+++ lib/services/app_router.dart
@@ -6,9 +6,13 @@ import 'package:capture/features/auth/cubit/sign_in_cubit.dart';
 import 'package:capture/features/auth/presentations/sign_in_page.dart';
 import 'package:capture/features/auth/presentations/sign_up_page.dart';
 import 'package:capture/features/auth/repository/auth_repositry.dart';
+import 'package:capture/features/history/presentations/history_page.dart';
 import 'package:capture/features/home/presentations/home_page.dart';
+import 'package:capture/features/notification/presentations/notification_page.dart';
+import 'package:capture/features/profile/presentations/profile_page.dart';
 import 'package:capture/features/starter/presentations/onboard_page.dart';
 import 'package:capture/main.dart';
+import 'package:capture/widgets/bottom_navigation_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
@@ -19,9 +23,90 @@ class AppRouter {
 
   static final GlobalKey<NavigatorState> parentNavigatorKey =
       GlobalKey<NavigatorState>();
+  static final GlobalKey<NavigatorState> homeTabNavigatorKey =
+      GlobalKey<NavigatorState>();
+  static final GlobalKey<NavigatorState> historyTabNavigatorKey =
+      GlobalKey<NavigatorState>();
+  static final GlobalKey<NavigatorState> notifTabNavigatorKey =
+      GlobalKey<NavigatorState>();
+  static final GlobalKey<NavigatorState> profileTabNavigatorKey =
+      GlobalKey<NavigatorState>();
 
   AppRouter(this._authCubit) {
-    final routes = [
+    final routes = <RouteBase>[
+      StatefulShellRoute.indexedStack(
+        parentNavigatorKey: parentNavigatorKey,
+        branches: [
+          StatefulShellBranch(
+            navigatorKey: homeTabNavigatorKey,
+            routes: [
+              GoRoute(
+                path: Destination.homePath,
+                pageBuilder: (context, GoRouterState state) {
+                  return getPage(
+                    child: const HomePage(),
+                    state: state,
+                  );
+                },
+              ),
+            ],
+          ),
+          StatefulShellBranch(
+            navigatorKey: historyTabNavigatorKey,
+            routes: [
+              GoRoute(
+                path: Destination.historyPath,
+                pageBuilder: (context, GoRouterState state) {
+                  return getPage(
+                    child: const HistoryPage(),
+                    state: state,
+                  );
+                },
+              ),
+            ],
+          ),
+          StatefulShellBranch(
+            navigatorKey: notifTabNavigatorKey,
+            routes: [
+              GoRoute(
+                path: Destination.notifPath,
+                pageBuilder: (context, GoRouterState state) {
+                  return getPage(
+                    child: const NotificationPage(),
+                    state: state,
+                  );
+                },
+              ),
+            ],
+          ),
+          StatefulShellBranch(
+            navigatorKey: profileTabNavigatorKey,
+            routes: [
+              GoRoute(
+                path: Destination.profilePath,
+                pageBuilder: (context, GoRouterState state) {
+                  return getPage(
+                    child: const ProfilePage(),
+                    state: state,
+                  );
+                },
+              ),
+            ],
+          ),
+        ],
+        pageBuilder: (
+          BuildContext context,
+          GoRouterState state,
+          StatefulNavigationShell navigationShell,
+        ) {
+          return getPage(
+            child: BottomNavigationPage(
+              child: navigationShell,
+            ),
+            state: state,
+          );
+        },
+      ),
       GoRoute(
         parentNavigatorKey: parentNavigatorKey,
         path: Destination.homePath,
@@ -128,6 +213,9 @@ class Destination {
 
   // Need Auth
   static const String homePath = '/home';
+  static const String historyPath = '/history';
+  static const String notifPath = '/notif';
+  static const String profilePath = '/profile';
 }
 
 class GoRouterRefreshStream extends ChangeNotifier {
